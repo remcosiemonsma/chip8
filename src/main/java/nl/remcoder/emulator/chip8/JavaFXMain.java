c060ecd50cc24afc16730c1312e8c120601472df
Remco Siemonsma <remco.siemonsma@nxp.com>
2020-11-18 17:15:53 +0100

    Ongoing work to handle keyboard inpout

--- src/main/java/nl/remcoder/emulator/chip8/JavaFXMain.java
+++ src/main/java/nl/remcoder/emulator/chip8/JavaFXMain.java
@@ -1,49 +1,41 @@
 package nl.remcoder.emulator.chip8;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class JavaFXMain extends Application {
 
     private CPUThread cpuThread;
+    private GraphicsContext gc;
+    private Stage stage;
+    private Canvas canvas;
 
     @Override
-    public void start(Stage stage) throws URISyntaxException, IOException {
+    public void start(Stage stage) throws URISyntaxException {
+        this.stage = stage;
         var root = new VBox();
-        var canvas = new Canvas(640, 320);
-        var gc = canvas.getGraphicsContext2D();
+        canvas = new Canvas(640, 320);
+        gc = canvas.getGraphicsContext2D();
 
-        Menu fileMenu = new Menu("File");
-        MenuItem openMenuItem = new MenuItem("Open");
-        fileMenu.getItems().add(openMenuItem);
-
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Image");
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All Files", "*.*"));
-        //Adding action on the menu item
-        openMenuItem.setOnAction(event -> {
-            //Opening a dialog box
-            fileChooser.showOpenDialog(stage);
-        });
-
-        MenuBar menuBar = new MenuBar(fileMenu);
+        MenuBar menuBar = createMenu();
 
         root.getChildren().add(menuBar);
         root.getChildren().add(canvas);
@@ -52,14 +44,55 @@ public class JavaFXMain extends Application {
 
         var scene = new Scene(root, Color.BLACK);
 
-        stage.setTitle("Lines");
+        stage.setTitle("Remcoders CHIP8 Emulator");
         stage.setScene(scene);
         stage.show();
 
-        cpuThread = new CPUThread(gc);
-        Thread thread = new Thread(cpuThread);
+        loadRom(Path.of(ClassLoader.getSystemResource("roms/15PUZZLE").toURI()));
+    }
 
-        thread.start();
+    private MenuBar createMenu() {
+        Menu fileMenu = new Menu("File");
+        MenuItem openMenuItem = new MenuItem("Open");
+        openMenuItem.setOnAction(event -> selectAndLoadRom());
+        fileMenu.getItems().add(openMenuItem);
+        return new MenuBar(fileMenu);
+    }
+
+    private void selectAndLoadRom() {
+        Path pathToRom = selectRom();
+        loadRom(pathToRom);
+    }
+
+    private Path selectRom() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Image");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All Files", "*.*"));
+        return fileChooser.showOpenDialog(stage).toPath();
+    }
+
+    private void loadRom(Path pathToRom) {
+        if (cpuThread != null && cpuThread.isRunning()) {
+            cpuThread.stop();
+        }
+        try {
+            cpuThread = new CPUThread(gc, pathToRom);
+            canvas.setOnKeyPressed(keyEvent -> cpuThread.keyEventHandler(keyEvent));
+            Thread thread = new Thread(cpuThread);
+            thread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            Stage dialogStage = new Stage();
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+
+            VBox vbox = new VBox(new Text("An error occured:\n" + e.getMessage()), new Button("Ok."));
+            vbox.setAlignment(Pos.CENTER);
+            vbox.setPadding(new Insets(15));
+
+            dialogStage.setScene(new Scene(vbox));
+            dialogStage.show();
+        }
     }
 
     @Override
@@ -70,56 +103,5 @@ public class JavaFXMain extends Application {
 
     public static void main(String[] args) {
         launch();
-    }
-
-    private static class CPUThread implements Runnable {
-
-        private final CPU cpu;
-        private final GraphicsContext graphicsContext;
-        private boolean running = true;
-
-        public CPUThread(GraphicsContext graphicsContext) throws URISyntaxException, IOException {
-            this.graphicsContext = graphicsContext;
-            cpu = new CPU();
-            cpu.initialize();
-
-            cpu.loadRom(Files.readAllBytes(Path.of(ClassLoader.getSystemResource("roms/15PUZZLE").toURI())));
-        }
-
-        public void stop() {
-            running = false;
-        }
-
-        @Override
-        public void run() {
-            while(running) {
-                cpu.emulateCycle();
-                paintScreen(cpu.getGraphics(), graphicsContext);
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        private void paintScreen(boolean[][] graphics, GraphicsContext graphicsContext) {
-            for (int y = 0; y < 32; y++) {
-                for (int x = 0; x < 64; x++) {
-                    boolean value = graphics[y][x];
-                    paintPixel(value, x, y, graphicsContext);
-                }
-            }
-        }
-
-        private void paintPixel(boolean white, int x, int y, GraphicsContext graphicsContext) {
-            if (white) {
-                graphicsContext.setFill(Color.LIMEGREEN);
-            } else {
-                graphicsContext.setFill(Color.BLACK);
-            }
-
-            graphicsContext.fillRect(x * 10, y * 10, 10, 10);
-        }
     }
 }
