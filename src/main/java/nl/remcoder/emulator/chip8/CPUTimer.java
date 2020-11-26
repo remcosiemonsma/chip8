2846b6178839ddfa2316ecd8af8d397aed524a6b
Remco Siemonsma <remco.siemonsma@cgi.com>
2020-11-26 21:51:19 +0100

    - Added support for audio output

--- src/main/java/nl/remcoder/emulator/chip8/CPUTimer.java
+++ src/main/java/nl/remcoder/emulator/chip8/CPUTimer.java
@@ -2,22 +2,35 @@ package nl.remcoder.emulator.chip8;
 
 import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.media.AudioClip;
 import javafx.scene.paint.Color;
 
 public class CPUTimer extends AnimationTimer {
 
     private final CPU cpu;
     private final GraphicsContext graphicsContext;
+    private final AudioClip beepAudio;
 
     public CPUTimer(GraphicsContext graphicsContext, CPU cpu) {
         this.graphicsContext = graphicsContext;
         this.cpu = cpu;
+        beepAudio = new AudioClip(ClassLoader.getSystemResource("Beep.wav").toString());
+        beepAudio.setCycleCount(AudioClip.INDEFINITE);
     }
 
     @Override
     public void handle(long now) {
         cpu.emulateCycle();
         paintScreen(cpu.getGraphics(), graphicsContext);
+        if (!beepAudio.isPlaying() && cpu.getSound_timer() > 0) {
+            beepAudio.play();
+        }
+        if (beepAudio.isPlaying() && cpu.getSound_timer() == 0) {
+            beepAudio.stop();
+        }
+        if (cpu.getSound_timer() > 0) {
+            cpu.setSound_timer(cpu.getSound_timer() - 1);
+        }
     }
 
     private void paintScreen(boolean[][] graphics, GraphicsContext graphicsContext) {
